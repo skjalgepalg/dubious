ce491e67f176ac3e8b03e2dc6fc5095a5f1a751d
Skjalg Erling Schrødter Teig <ste@capraconsulting.no>
2018-03-13 22:37:36 +0100

    Add practice hints to SupremeHeader

--- src/modules/recompose/SupremeHeader.js
+++ src/modules/recompose/SupremeHeader.js
@@ -1,23 +1,53 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes, { func, bool } from 'prop-types'
 import { withState, withHandlers, compose, withProps } from 'recompose'
 import onClickOutside from 'react-onclickoutside'
 
 const SUPREME_SOURCE = 'https://avatars2.githubusercontent.com/u/430645?s=400&v=4'
 const PLAIN_SOURCE = 'https://avatars3.githubusercontent.com/u/2345608?s=460&v=4'
 
+// Javascript function hoisting -> proptypes first <3
+NamedHeader.propTypes = {
+  handleClick: func,
+  isSupremeLeader: bool,
+}
+
+// Named function
+function NamedHeader({ handleClick, isSupremeLeader }) {
+  return (
+    <header className='App-header'>
+      <figure>
+        <img
+          src={isSupremeLeader ? SUPREME_SOURCE : PLAIN_SOURCE}
+          className='App-logo'
+          alt='supremeheader'
+          onClick={handleClick}
+        />
+      </figure>
+      <h1 className='App-title'>
+        {isSupremeLeader
+          ? 'Best practices'
+          : 'Dubious React practices'
+        }
+      </h1>
+    </header>
+  )
+}
+
+
+// Anonymous function (avoid, but oooh so ES6 and modern)
 const SupremeHeader = ({
-  isSupremeLeader,
   handleClick,
+  isSupremeLeader,
 }) =>
   <header className='App-header'>
     <figure>
       <img
-        src={isSupremeLeader
-          ? SUPREME_SOURCE
-          : PLAIN_SOURCE}
+        src={isSupremeLeader ? SUPREME_SOURCE : PLAIN_SOURCE}
         className='App-logo'
         alt='supremeheader'
+        // onChange={(e) => { model.name = e.target.value }}
+        // Avoid creating new closures in the render method- use methods like below
         onClick={handleClick}
       />
     </figure>
@@ -30,8 +60,8 @@ const SupremeHeader = ({
   </header>
 
 SupremeHeader.propTypes = {
-  isSupremeLeader: PropTypes.bool,
   handleClick: PropTypes.func,
+  isSupremeLeader: PropTypes.bool,
 }
 
 export default compose(
