ff4c4e51d79a633c68c0f98342b2f1bf4812e5fc
Skjalg Erling Schrødter Teig <ste@capraconsulting.no>
2018-03-13 22:34:48 +0100

    Set app to base state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import './App.css'
-// 1: Rydd opp
+import logo from './logo.svg'
+// 1: Del opp
 // import Header from './components/Header'
 // 2: Recompose
 // import SupremeHeader from './modules/recompose/SupremeHeader'
 // 3: properties og proptypes
+// 4. renderprops
+// import GeoPosition from './modules/properties/GeoPosition';
 // 4: Latskap og vedlikehold
 // Incoming
 
 // class App extends Component {
 //   render() {
 //     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
+//       <div className='App'>
+//         <header className='App-header'>
+//           <img src={logo} className='App-logo' alt='logo' />
+//           <h1 className='App-title'>Welcome to React</h1>
 //         </header>
-//         <p className="App-intro">
+//         <p className='App-intro'>
 //           To get started, edit <code>src/App.js</code> and save to reload.
 //         </p>
 //       </div>
@@ -26,19 +29,27 @@ import './App.css'
 
 const App = () =>
   <div className='App'>
-    {/* <header className='App-header'>
+    <header className='App-header'>
       <img
-        src='https://avatars3.githubusercontent.com/u/2345608?s=460&v=4'
+        src={logo}
         className='App-logo'
         alt='Wow such logo'
       />
       <h1 className='App-title'>Various React practices</h1>
-    </header> */}
+    </header>
     {/* <Header /> */}
     {/* <SupremeHeader /> */}
     <p className='App-intro'>
-      Tilgi oss ikke, for vi vet noenlunde hva vi driver med
+      Tilgi oss ikke, for vi vet sånn noenlunde hva vi driver med
     </p>
+
+    {/* <GeoPosition
+      render={state => (
+        <div>
+          Lat: {state.lat || '...'} Long: {state.lng || '...'}
+        </div>
+      )}
+    /> */}
   </div>
 
 export default App
